e375a846ce8d80687f36b35a807a82b98f1bb2f0
fukadashigeru <sakusaku721@gmail.com>
2020-10-12 16:38:35 +0900

    04~08

---
+++ javascript_basics/js/main_8.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log("It's me!");
+console.log('It\'s me!');
+
+console.log('hel\nlo wor\tld');
+console.log('hello' + 'world');
+
+let price = 150;
+
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
+
+console.log(price * 140);
+console.log(price * 160);
