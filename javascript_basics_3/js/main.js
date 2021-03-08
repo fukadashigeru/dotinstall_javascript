60b03d29cb9c580c4d29017132ca3fcaaac8ef8e
fukadashigeru <sakusaku721@gmail.com>
2021-03-08 22:44:23 +0900

    #08 classListを使ってみよう

--- javascript_basics_3/js/main.js
+++ javascript_basics_3/js/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
+// {
+//   console.log('#05 addEventListener()を使ってみよう');
+
+//   document.querySelector('button').addEventListener('click', () => {
+//     document.getElementById('target').textContent = 'Changed';
+//   });
+// }
+
 {
-  console.log('#05 addEventListener()を使ってみよう');
+  console.log('#08 classListを使ってみよう');
 
   document.querySelector('button').addEventListener('click', () => {
-    document.getElementById('target').textContent = 'Changed';
-  });
+    const targetNode = document.getElementById('target');
+    targetNode.classList.toggle('my-color');
+  })
 }
-
